9634ae4100d1c704dfdd27a18288d25dc9d90a37
roseee123 <m10702150@gapps.ntust.edu.tw>
2019-06-21 20:54:31 +0800

    set routes

--- routes/web.php
+++ routes/web.php
@@ -11,6 +11,36 @@
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
+//Route::get('/', function () {
+//    return view('welcome');
+//});
+
+//首頁
+Route::get('/', 'MerchandiseController@IndexPage');
+//使用者
+Route::group(['prefix' => 'user'], function () {
+    Route::group(['prefix' => 'auth'], function () {
+        Route::get('/sign-up', 'UserAuthController@signUpPage');
+        Route::post('/sign-up', 'UserAuthController@signUpProcess');
+        Route::get('/sign-in', 'UserAuthController@signInPage');
+        Route::post('/sign-in', 'UserAuthController@signInProcess');
+        Route::get('/sign-out', 'UserAuthController@signOut');
+    });
 });
+
+//商品
+Route::group(['prefix' => 'merchandise'], function () {
+    Route::get('/', 'MerchandiseController@merchandiseListPage');
+    Route::get('/create', 'MerchandiseController@merchandiseCreateProcess');
+    Route::get('/manage', 'MerchandiseController@merchandiseManageListPage');
+    Route::group(['prefix' => '{merchandise_id}'], function () {
+        Route::get('/', 'MerchandiseController@merchandiseItemPage');
+        Route::get('/edit', 'MerchandiseController@merchandiseItemEditPage');
+        Route::put('/', 'MerchandiseController@merchandiseItemUpdateProcess');
+        Route::post('/buy', 'MerchandiseController@merchandiseItemBuyProcess');
+    });
+});
+
+//交易
+Route::get('/transaction', 'TransactionController@transactionListPage');
+
